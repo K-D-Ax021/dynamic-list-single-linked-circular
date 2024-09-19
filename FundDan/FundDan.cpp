ce6c29b345eeba8e0ff38f192c24dbe35c4ca786
steam <steam@MAYHEM>
2024-09-19 20:29:49 +1000

    Добавьте файлы проекта.

---
+++ FundDan/FundDan.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+struct Node {
+    int x;
+    Node *Next;
+};
+
+class List {
+    Node* Head, * Tail;
+public:
+    List() :Head(NULL), Tail(NULL) {};
+    void Add(int x);
+};
+void List::Add(int x) {
+
+}
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
